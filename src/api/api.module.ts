d5e08b70352eb5200733f50c23a37c91100f5321
Michał Kamiński <michal.kaminski@nextbike.com>
2024-09-11 21:52:43 +0200

    enable-autoload-entities

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ApiController } from './api.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiService } from './api.service';
+import { ApiController } from './api.controller';
+import { Character, Episode, Quote, Tag, QuoteTag } from './entities';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Character, Episode, Quote, Tag, QuoteTag]),
+  ],
   controllers: [ApiController],
-  providers: [ApiService]
+  providers: [ApiService],
 })
 export class ApiModule {}
